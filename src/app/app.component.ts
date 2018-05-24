70c3ef6bc706db482f8cd160c35127830fc3f100
Luchillo <luchillo17@gmail.com>
2018-05-23 22:13:57 -0500

    [Refactor] Small style changes & refactors to variable names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component } from '@angular/core';
-import { hierarchy, HierarchyPointLink, HierarchyPointNode, select, tree } from 'd3';
+import { hierarchy, HierarchyPointLink, HierarchyPointNode, select, path, tree } from 'd3';
 
 import { AVLNode } from './models/avlnode';
 import { AVLTree } from './models/avltree';
@@ -22,15 +22,15 @@ export class AppComponent implements AfterViewInit {
     const treeLayout = tree<AVLNode>().size([xSize, ySize]);
 
     // Set svg sizes
-    const svg = select('#treeSVG')
-      .attr('viewBox', `0 0 ${xSize} ${ySize}`)
+    const treeSVG = select('#treeSVG')
+      .attr('viewBox', `-30 -30 ${xSize + 30} ${ySize + 60}`)
       .style('border', '1px solid black');
 
     // Generate tree layout
     const nodes = treeLayout(d3Tree);
 
     // Populate nodes with data
-    const nodeGroups = svg
+    const nodeGroups = treeSVG
       .selectAll('g.node')
       .data(nodes.descendants(), (d: HierarchyPointNode<AVLNode>) => d.data.value);
 
@@ -38,7 +38,7 @@ export class AppComponent implements AfterViewInit {
     const nodeGroupEnter = nodeGroups
       .enter()
       .append('g')
-      .attr('class', 'node')
+      .classed('node', true)
       .attr('transform', d => `translate(${d.x}, ${d.y})`);
 
     nodeGroupEnter.append('circle');
@@ -49,21 +49,17 @@ export class AppComponent implements AfterViewInit {
       .text(d => d.data.value);
 
     // Populate Links elements
-    const links = svg
+    const links = treeSVG
       .selectAll('line.link')
       .data(nodes.links(), (d: HierarchyPointLink<AVLNode>) => d.target.data.value);
 
     links
       .enter()
       .insert('line', 'g')
-      .attr('class', 'link')
+      .classed('link', true)
       .attr('x1', d => d.source.x)
       .attr('y1', d => d.source.y)
       .attr('x2', d => d.target.x)
       .attr('y2', d => d.target.y);
-  }
-
-  getX(d: HierarchyPointLink<AVLNode>) {
-    const r = 20;
   }
 }
