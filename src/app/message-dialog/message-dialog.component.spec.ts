3023fc58830e251589ff26f988f4d74a98ab963e
Luchillo <luchillo17@gmail.com>
2018-06-04 14:24:50 -0500

    [Test] Fix dialog tests & remove jasmine deps

--- src/app/message-dialog/message-dialog.component.spec.ts
+++ src/app/message-dialog/message-dialog.component.spec.ts
@@ -1,26 +1,71 @@
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { Component, NgModule } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialogModule } from '@angular/material';
+import { MatDialog, MatDialogConfig, MatDialogModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MessageDialogComponent } from './message-dialog.component';
+import { MessageDialogData } from './message-dialog.interfaces';
 
+// Noop component to trigger change detection
+@Component({
+  selector: 'app-noop',
+  template: '',
+})
+export class NoopComponent {}
+
+const testComponents = [MessageDialogComponent, NoopComponent];
+
+// Dummy module for dialog testing, needed to have entryComponents
+@NgModule({
+  declarations: testComponents,
+  imports: [MatDialogModule, NoopAnimationsModule],
+  exports: testComponents,
+  entryComponents: [MessageDialogComponent],
+})
+export class DialogTestingModule {}
+
+/**
+ * Tests start from here, import the DialogTestingModule & provide a
+ * container element for the dialog,which will be used later to
+ * access the html of the dialog later for the tests.
+ */
 describe('MessageDialogComponent', () => {
-  let component: MessageDialogComponent;
-  let fixture: ComponentFixture<MessageDialogComponent>;
+  let dialog: MatDialog;
+  let noopComponent: ComponentFixture<NoopComponent>;
+  let overlayContainerElement: HTMLDivElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [MessageDialogComponent],
-      imports: [MatDialogModule],
+      declarations: [],
+      imports: [DialogTestingModule],
+      providers: [
+        {
+          // Provide a container for the dialog, save the container in variable for css queries later
+          provide: OverlayContainer,
+          useFactory: () => {
+            overlayContainerElement = document.createElement('div');
+            return { getContainerElement: () => overlayContainerElement };
+          },
+        },
+      ],
     }).compileComponents();
+
+    dialog = TestBed.get(MatDialog);
+
+    noopComponent = TestBed.createComponent(NoopComponent);
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MessageDialogComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  it('Show information title & message', () => {
+    const data: MessageDialogData = { title: 'The title', text: 'The text' };
+    const config: MatDialogConfig = { data };
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    dialog.open(MessageDialogComponent, config);
+
+    // Trigger change detection on TestBed for the dialog to update
+    noopComponent.detectChanges();
+
+    expect(overlayContainerElement.querySelector('[mat-dialog-title]').textContent).toBe(data.title);
+    expect(overlayContainerElement.querySelector('p').textContent).toBe(data.text);
   });
 });
